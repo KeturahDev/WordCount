6986824b76be66a5444b4254dfee3737bf44bded
Keturah Howard <keturah.dev@gmail.com>
2020-03-06 13:15:39 -0800

    create/fail spec4-test: establish counting method

--- WordCounter.Tests/ModelTests/CounterTests.cs
+++ WordCounter.Tests/ModelTests/CounterTests.cs
@@ -19,5 +19,21 @@ namespace WordCounter.Tests
 
       Assert.AreEqual("If",list[0]);
     }
+    [TestMethod]
+    public void CountingWord_CountsOccurancesOfKeyInSentence_int()
+    {
+      string usersSentence = "If I were a rich man la la la la la la la la la la la";
+      string usersKeyWord = "la";
+      Word userWord = new Word(usersKeyWord);
+      Sentence lyrics = new Sentence(usersSentence);
+      Counter newCounter = new Counter(userWord, lyrics);
+      string[] list = newCounter.GenerateArray();
+      newCounter.Counting(list);
+
+      int numberOfOccurences = newCounter.Count;
+
+      Assert.AreEqual(11, numberOfOccurences);
+
+    }
   }
 }
--- WordCounter/Models/Counter.cs
+++ WordCounter/Models/Counter.cs
@@ -4,16 +4,22 @@ namespace WordCounter.Models
   {
     public string CheckFor {get;set;}
     public string Sentence {get;set;}
+    public int Count {get;set;}
     public Counter(Word word, Sentence sentence)
     {
       CheckFor = word.Key;
       Sentence = sentence.Value;
+      Count = 0;
     }
 
     public string[] GenerateArray()
     {
       string[] stringArray = Sentence.Split(" ");
       return stringArray;
+    }
+    public void Counting(string[] listOfWords)
+    {
+      Count = Count += 0;
     }
   }
 }
