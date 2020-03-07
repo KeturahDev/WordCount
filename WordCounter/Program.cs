a52b6b0234e1b395b1c2c20305ef1cb1fe8cc0e0
Keturah Howard <keturah.dev@gmail.com>
2020-03-06 18:55:51 -0800

    create styling for console UI

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -12,13 +12,25 @@ namespace WordCounter
     }
     public static void Welcome()
     {
-      Console.WriteLine("WordCounter");
+      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.WriteLine(@"██╗    ██╗ ██████╗ ██████╗ ██████╗  ██████╗ ██████╗ ██╗   ██╗███╗   ██╗████████╗███████╗██████╗ 
+██║    ██║██╔═══██╗██╔══██╗██╔══██╗██╔════╝██╔═══██╗██║   ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗
+██║ █╗ ██║██║   ██║██████╔╝██║  ██║██║     ██║   ██║██║   ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝
+██║███╗██║██║   ██║██╔══██╗██║  ██║██║     ██║   ██║██║   ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗
+╚███╔███╔╝╚██████╔╝██║  ██║██████╔╝╚██████╗╚██████╔╝╚██████╔╝██║ ╚████║   ██║   ███████╗██║  ██║
+ ╚══╝╚══╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝");
+      Console.ForegroundColor = ConsoleColor.DarkBlue;
       Console.WriteLine("Where you can enter a sentence, a word you want to count, and we'll count it for you!");
     }
     public static void Start()
     {
+      Console.ForegroundColor = ConsoleColor.Cyan;
+      Console.WriteLine("================================================================================================");
+      Console.ForegroundColor = ConsoleColor.Blue;
       Console.WriteLine("Wound you like to go enter word or exit the application");
+      Console.ForegroundColor = ConsoleColor.Yellow;
       Console.Write("Continue [C]     Exit [E]    ");
+      Console.ForegroundColor = ConsoleColor.Green;
       string response = Console.ReadLine().ToLower();
 
       if(response == "c")
@@ -36,16 +48,22 @@ namespace WordCounter
     }
     public static void CountWord()
     {
+      Console.ForegroundColor = ConsoleColor.Blue;
       Console.Write("Please enter the word to check for:   ");
+      Console.ForegroundColor = ConsoleColor.Green;
       string usersWord = Console.ReadLine();
+      Console.ForegroundColor = ConsoleColor.Blue;
       Console.Write("Please enter the sentence to check:   ");
-      string usersSentence = Console.ReadLine();
+      Console.ForegroundColor = ConsoleColor.Green;
+      string usersSentence = Console.ReadLine().ToLower();
       Input userInputs = new Input(usersWord ,usersSentence);
       Counter newCounter = new Counter(userInputs);
-
+      newCounter.IgnoreExpressions();
+      // Console.WriteLine($">>>>   SENTENCE AFTER IEMETHOD:{newCounter.Sentence}");
       string[] list = newCounter.GenerateArray();
       newCounter.Counting(list);
 
+      Console.ForegroundColor = ConsoleColor.Magenta;
       Console.WriteLine($"Your word {userInputs.Key} is in that sentence {newCounter.Count} times");
       Start();
     }
